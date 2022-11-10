8273976c34bdeec50fa9ec36438b95a3af4297ac
spr1ng <iamspase@protonmail.com>
2022-11-10 21:45:32 +0100

    Setup MongoDB database

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import express, { Application } from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config({path: "src/.env"});
 
 const app: Application = express();
 
-app.listen(8000, () => {
+const PORT: number = 8080;
+const MONGO_URI: string = process.env.MONGO_URI!;
+
+mongoose.connect(MONGO_URI).then(() => {
+    console.log("Database running.");
+}).catch((error) => {
+    console.log(error);
+})
+
+app.listen(PORT, () => {
     console.log("Server up.");
 })
